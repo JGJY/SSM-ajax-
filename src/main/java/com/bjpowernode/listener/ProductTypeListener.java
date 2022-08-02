a6726537f362497535c457c11fc4dd08b229f6f3
wangfen <2308553780@qq.com>
2022-08-02 09:37:42 +0800

    第一次提交

---
+++ src/main/java/com/bjpowernode/listener/ProductTypeListener.java
@@ -0,0 +1,31 @@
+package com.bjpowernode.listener;
+
+import com.bjpowernode.pojo.ProductType;
+import com.bjpowernode.service.ProductTypeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.List;
+@WebListener
+public class ProductTypeListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        //手工从Spring容器中取出ProductTypeServiceImpl的对象
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_*.xml");
+        ProductTypeService productTypeService = (ProductTypeService) context.getBean("ProductTypeServiceImpl");
+        List<ProductType> typeList = productTypeService.getAll();
+        typeList.forEach(productType-> System.out.println(productType));
+        System.out.println("1111111111111111111111111");
+        //放入全局作用域中，供应所有功能使用
+        servletContextEvent.getServletContext().setAttribute("typeList",typeList);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
